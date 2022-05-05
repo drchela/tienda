76eb8dc2be85fcdd29eba7a705f7311f667d9e2b
Jose Enrique Ceballos Martinez <44713568+drchela@users.noreply.github.com>
2022-05-04 20:41:40 -0500

    Update index.blade.php

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -17,47 +17,6 @@
     <link type="text/css" href="{{ asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
            <!-- Extra details for Live View on GitHub Pages -->
     <!-- Canonical SEO -->
-    <link rel="canonical" href="https://www.creative-tim.com/product/argon-dashboard-laravel" />
-    <!--  Social tags      -->
-    <meta name="keywords" content="dashboard, bootstrap 4 dashboard, bootstrap 4 design, bootstrap 4 system, bootstrap 4, bootstrap 4 uit kit, bootstrap 4 kit, argon, argon ui kit, creative tim, html kit, html css template, web template, bootstrap, bootstrap 4, css3 template, frontend, responsive bootstrap template, bootstrap ui kit, responsive ui kit, argon dashboard">
-    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
-    <!-- Schema.org markup for Google+ -->
-    <meta itemprop="name" content="Argon - Free Dashboard for Bootstrap 4 by Creative Tim">
-    <meta itemprop="description" content="Start your development with a Dashboard for Bootstrap 4.">
-    <meta itemprop="image" content="https://s3.amazonaws.com/creativetim_bucket/products/96/original/opt_ad_thumbnail.jpg">
-    <!-- Twitter Card data -->
-    <meta name="twitter:card" content="product">
-    <meta name="twitter:site" content="@creativetim">
-    <meta name="twitter:title" content="Argon - Free Dashboard for Bootstrap 4 by Creative Tim">
-    <meta name="twitter:description" content="Start your development with a Dashboard for Bootstrap 4.">
-    <meta name="twitter:creator" content="@creativetim">
-    <meta name="twitter:image" content="https://s3.amazonaws.com/creativetim_bucket/products/96/original/opt_ad_thumbnail.jpg">
-    <!-- Open Graph data -->
-    <meta property="fb:app_id" content="655968634437471">
-    <meta property="og:title" content="Argon - Free Dashboard for Bootstrap 4 by Creative Tim" />
-    <meta property="og:type" content="article" />
-    <meta property="og:url" content="https://demos.creative-tim.com/argon-dashboard/index.html" />
-    <meta property="og:image" content="https://s3.amazonaws.com/creativetim_bucket/products/96/original/opt_ad_thumbnail.jpg" />
-    <meta property="og:description" content="Start your development with a Dashboard for Bootstrap 4." />
-    <meta property="og:site_name" content="Creative Tim" />
-    <!-- Google Tag Manager -->
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-NKDMSK6');
-    </script>
-    <!-- End Google Tag Manager -->
 </head>
 <body class="clickup-chrome-ext_installed">
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -103,6 +62,7 @@
                     <i class="ni ni-support-16"></i>
                     <span>Support</span>
                 </a>
+                <p></p>
                 <div class="dropdown-divider"></div>
                 <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
                 document.getElementById('logout-form').submit();">
@@ -398,28 +358,24 @@
                             <tr>
                                 <th scope="col">Nombre</th>
                                 <th scope="col">Tipo</th>
+                                <th scope="col">Precio</th>
+                                <th scope="col">Tamaño</th>
                                 <th scope="col">Sku</th>
                                 <th scope="col">Fecha</th>
                             </tr>
                         </thead>
                         <tbody>
-                                                                <tr>
-                                    <td>Admin Admin</td>
-                                    <td>
-                                        <a href="mailto:admin@argon.com">admin@argon.com</a>
-                                    </td>
-                                    <td>12/02/2020 11:00</td>
-                                    <td class="text-right">
-                                        <div class="dropdown">
-                                            <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                <i class="fas fa-ellipsis-v"></i>
-                                            </a>
-                                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                                                                                        <a class="dropdown-item" href="">Edit</a>
-                                                                                                </div>
-                                        </div>
-                                    </td>
-                                </tr>
+                            @foreach ($productos as $product )
+                            <tr>
+                                <td>{{$product->name}}</td>
+                                <td> {{$product->type}}</td>
+                                <td>{{$product->price}}</td>
+                                <td>{{$product->size}}</td>
+                                <td>{{$product->sku}}</td>
+                                <td>{{$product->created_at}}</td>
+                            </tr>
+
+                            @endforeach
                                                         </tbody>
                     </table>
                 </div>
